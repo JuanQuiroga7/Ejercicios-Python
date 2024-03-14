c8551119c1b4ce9c7ba9316eb172b60d1e128df7
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-03-14 14:07:00 -0500

    Teorema de Pitagoras

---
+++ Eje6.py
@@ -0,0 +1,6 @@
+cateto_a = float(input("Ingrese el cateto a: "))
+cateto_b = float(input("Ingrese el cateto b: "))
+
+teoremapi = (cateto_a**2 + cateto_b**2)**0.5
+
+print("El resultado para el teorema de Pitagoras es: ",teoremapi)
