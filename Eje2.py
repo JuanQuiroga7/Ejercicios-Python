146429e3f4754cc8e8e8cc6bf1da0bdc9e539e9c
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-03-14 13:41:57 -0500

    perimetro y area de un circulo

---
+++ Eje2.py
@@ -0,0 +1,6 @@
+radio = float(input("Ingrese el radio: "))
+perimetro = (2 * 3.14) * radio
+area = 3.14 * radio * radio 
+
+print("El perimetro del circulo es: ", perimetro)
+print("El area del circulo es: ", area)
