c61f8f008499ea37636edface4775861872610dd
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-03-14 20:42:08 -0500

    Ordenamiento 2

--- Eje15.py
+++ Eje15.py
@@ -1,7 +1,8 @@
 num1 = int(input("Ingrese el primer numero: "))
 num2 = int(input("Ingrese el segundo numero: "))
+num3 = int(input("Ingrese el tercer numero: "))
 
-comp = [num1, num2]
+comp = [num1, num2, num3]
 
 comp.sort()
 
